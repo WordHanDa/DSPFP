0d170c77c75eaddf8b5a77805a8ff5173b5c27ff
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2024-05-26 16:55:30 +0800

    9

--- main.py
+++ main.py
@@ -1,173 +1,265 @@
+
+# encoding: utf-8
+
+import os
+import random
+import sys
+
 import pygame
-import time
-from random import randint
-#視窗大小
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 400
-#常用顏色
-GREEN = (73,188,11)
-WHITE = (255,255,255)
-#地鼠座標 / 分數 /遊戲時間 / 開始時間 / 狀態
-x,y = None,None
-score = 0
-game_time = 20
-start_time = 0
-state = 0 #首頁0 遊戲中1 結束2
-#建立視窗及頻率鐘
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-pygame.display.set_caption('打地鼠')
+from pygame.locals import *
+
+from drew import *
+
+# 視窗大小.
+canvas_width = 800
+canvas_height = 600
+
+# 顏色.
+block = (0,0,0)
+
+# 磚塊數量串列.
+bricks_list = []
+
+# 移動速度.
+dx =  8
+dy = -8
+
+# 遊戲狀態.
+# 0:等待開球
+# 1:遊戲進行中
+game_mode = 0
+
+#-------------------------------------------------------------------------
+# 函數:秀字.
+#-------------------------------------------------------------------------
+def showFont( text, x, y, color):
+    global canvas    
+    text = font.render(text, True, color) 
+    canvas.blit( text, (x,y))
+
+#-------------------------------------------------------------------------
+# 函數:碰撞判斷.
+#   x       : x 
+#   y       : y 
+#   boxRect : 矩形
+#-------------------------------------------------------------------------
+def isCollision( x, y, boxRect):
+    if (x >= boxRect[0] and x <= boxRect[0] + boxRect[2] and y >= boxRect[1] and y <= boxRect[1] + boxRect[3]):
+        return True;          
+    return False;  
+
+#-------------------------------------------------------------------------
+# 函數:初始遊戲.
+#-------------------------------------------------------------------------
+def resetGame():
+    # 宣告使用全域變數.
+    global game_mode, brick_num, bricks_list, dx, dy
+
+    # 磚塊
+    for bricks in bricks_list:
+        # 亂數磚塊顏色
+        r = random.randint(100,200)
+        g = random.randint(100,200)
+        b = random.randint(100,200)
+        bricks.color = [r,g,b]        
+        # 開啟磚塊.
+        bricks.visivle = True
+    # 0:等待開球
+    game_mode = 0
+    # 磚塊數量.
+    brick_num = 99    
+    # 移動速度.
+    dx =  8
+    dy = -8
+
+# 初始.
+pygame.init()
+# 顯示Title.
+pygame.display.set_caption(u"打磚塊遊戲")
+# 建立畫佈大小.
+canvas = pygame.display.set_mode((canvas_width, canvas_height))
+# 時脈.
 clock = pygame.time.Clock()
-#載入遊戲圖片
-mallet = pygame.image.load('mallet.png') 
-down_mallet = pygame.image.load('down-mallet.png')
-mole = pygame.image.load('mole.png') 
-grass = pygame.image.load('grass.png') 
-#隱藏滑鼠座標顯示及初始化文字模組
-pygame.mouse.set_visible(False)
-pygame.font.init()
 
-# FPS
-FPS = 60
-# 歡迎畫面
-def welcome_screen():
-    # 全部填滿草
-    screen.blit(grass,(0,0))
-    # 字體設定
-    font = pygame.font.SysFont('corbel',48)
-    # 文字設定
-    text = font.render('Press ENTER to start',False,WHITE)
-    # 畫出文字部分
-    screen.blit(text,((SCREEN_WIDTH - text.get_width()) / 2, 185 ) )
-    #畫出一個地鼠
-    screen.blit(mole,(120,50))
-    # 取得槌子的矩形範圍
-    mallet_position = mallet.get_rect()
-    # 將槌子的中心點設在滑鼠點的位置
-    mallet_position.center = pygame.mouse.get_pos()
-    if pygame.mouse.get_pressed()[0]:
-         screen.blit(down_mallet, mallet_position)
-    else:
-         screen.blit(mallet, mallet_position)
-    
-def play():
-   # 取用遊戲狀態、分數及開始時間資訊
-   global state, score, start_time
-   # 設定遊戲開始時間 time.time() 會取得目前的時間
-   start_time = time.time()
-   # 將分數歸 0 且狀態設定為 1 遊玩中
-   score = 0
-   state = 1
-   # 產生新的老鼠（這邊先立一個函式之後完成）
-   new_mole()
-   # 產生瞬間先檢查是否有被打到（這邊先立一個函式之後完成）
-   whack()
-  
-def end():
-   # 狀態改為 2 結束遊戲
-   global state
-   state = 2
- 
-def new_mole():
-   # 隨機決定下一個老鼠產生的位置
-   global x, y
-   # x 從螢幕最左到右邊扣掉老鼠的寬都能取, y 則向下移 30 到底部扣掉老鼠的高都能取
-   x = randint(0, SCREEN_WIDTH - mole.get_width())
-   y = randint(30, SCREEN_HEIGHT - mole.get_height())
- 
-# 判斷是否在方形區塊內
-def isInRect(p,rect):
-    # 先取得點座標
-    x1, y1 = p
-    # 再取得方形座標
-    x2, y2, len, width = rect
-    # 如果不在區域內 就 return false
-    if(x1<x2 or x1 > x2+len) : return False
-    elif (y1<y2 or y1>y2+width) : return False
-    # else return true
-    else: return True
+# 設定字型.
+font = pygame.font.SysFont("simsunnsimsun", 18)
 
-def whack():
-   global score
-   # 取得滑鼠當前的位置
-   mx, my = pygame.mouse.get_pos()
-   # 取得老鼠的寬及高
-   width, height = mole.get_size()
-   # 將座標計算是不是點擊在老鼠的圖片上, 如果有的話要加分和產生下一隻新的
-   if isInRect((mx,my),(x,y,width,height)):
-       score += 1
-       new_mole()
-   
-# 遊戲畫面
-def play_screen():
-    # 畫出草
-    screen.blit(grass,(0,0))
-    # 字體設定
-    font = pygame.font.SysFont('corbel',30)
-    # 分數的文字
-    text_score = font.render(str(score),False,WHITE)
-    # 現在的時間
-    current = game_time - (time.time() - start_time)
-    # 如果時間結束 就跳到 end
-    if current <= 0:
-          end()
-    # 時間的文字
-    text_time = font.render(str(int(current)),False,WHITE)
-    # 如果按下了滑鼠
-    if pygame.mouse.get_pressed()[0]:
-        # 就在鼠標位置顯示下降的槌子
-        screen.blit(down_mallet, pygame.mouse.get_pos())
-    # 如果不是的話
-    else:
-        # 就在鼠標的位置顯示一般的槌子
-        screen.blit(mallet,pygame.mouse.get_pos())
-    # 顯示分數
-    screen.blit(text_score,(10,0))
-    #顯示時間
-    screen.blit(text_time,(370,0))
-    # 顯示地鼠
-    screen.blit(mole,(x,y))
-        
-# 結束話面
-def end_screen():
-    # 背景填滿綠色
-    screen.fill(GREEN)
-    # 設定字體樣板分別顯示遊戲結束、分數及重新開始按鈕
-    font = pygame.font.Font(None, 30)
-    game_over = font.render("GAME OVER", False, WHITE)
-    font = pygame.font.Font(None, 25)
-    # 分數
-    points = font.render("Score: " + str(score), False, WHITE)
-    font = pygame.font.Font(None, 22)
-    restart = font.render("Press ENTER to play again", False, WHITE)
-    # 將上述資訊顯示到螢幕上
-    screen.blit(game_over, (SCREEN_WIDTH / 2 - game_over.get_width() / 2, 100))
-    screen.blit(points, (SCREEN_WIDTH / 2 - points.get_width() / 2, 200))
-    screen.blit(restart, (SCREEN_WIDTH / 2 - restart.get_width() / 2, 300))
+# 底板.
+paddle_x = 0
+paddle_y = (canvas_height - 48)
+paddle = Box(pygame, canvas, "paddle", [paddle_x, paddle_y, 100, 24], (255,255,255))
 
-#遊戲執行
-running=True
+# 球.
+ball_x = paddle_x
+ball_y = paddle_y
+ball   = Circle(pygame, canvas, "ball", [ball_x, ball_x], 8, (255,255,255))
+
+# 建立磚塊
+brick_num = 0
+brick_x = 70
+brick_y = 60
+brick_w = 0
+brick_h = 0
+for i in range( 0, 99):
+    if((i % 11)==0):
+        brick_w = 0
+        brick_h = brick_h + 18        
+    bricks_list.append (Box(pygame, canvas, "brick_"+str(i), [  brick_w + brick_x, brick_h+ brick_y, 58, 16], [255,255,255]))
+    brick_w = brick_w + 60
+# 初始遊戲.
+resetGame()
+
+#-------------------------------------------------------------------------    
+# 主迴圈.
+#-------------------------------------------------------------------------
+running = True
 while running:
-    #事件處理
+    #---------------------------------------------------------------------
+    # 判斷輸入.
+    #---------------------------------------------------------------------
     for event in pygame.event.get():
-        #當遊戲視窗被關閉
-        if event.type==pygame.QUIT:#當遊戲視窗被關閉
-            running=False
-        elif state == 0:#遊戲還沒開始的事件
-           if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
-                    play()
-        elif state == 1:#遊戲中的事件
-            # 如果按下滑鼠 就打
-           if event.type == pygame.MOUSEBUTTONDOWN:
-               whack()
-        elif state == 2:#遊戲結束的事件
-             if event.type == pygame.KEYDOWN and event.key ==pygame.K_RETURN:
-                   play()
-        if state == 0:#還沒開始的畫面
-            welcome_screen()
-        elif state == 1:#遊戲中的畫面
-            play_screen()
-        elif state == 2:#遊戲結束的畫面
-            end_screen()
-        clock.tick(FPS)#限制畫面最高更新 60 FPS
-        pygame.display.update()#更新畫面      
+        # 離開遊戲.
+        if event.type == pygame.QUIT:
+            running = False
+        # 判斷按下按鈕
+        if event.type == pygame.KEYDOWN:
+            # 判斷按下ESC按鈕
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                
+        # 判斷Mouse.
+        if event.type == pygame.MOUSEMOTION:
+            paddle_x = pygame.mouse.get_pos()[0] - 50
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if(game_mode == 0):
+                game_mode = 1
+
+    #---------------------------------------------------------------------    
+    # 清除畫面.
+    canvas.fill(block)
+    
+    # 磚塊
+    for bricks in bricks_list:
+        # 球碰磚塊.
+        if(isCollision( ball.pos[0], ball.pos[1], bricks.rect)):
+            if(bricks.visivle):                
+                # 扣除磚塊.
+                brick_num = brick_num -1
+                # 初始遊戲.
+                if(brick_num <= 0):
+                    resetGame()
+                    break
+                # 球反彈.
+                dy = -dy; 
+            # 關閉磚塊.
+            bricks.visivle = False
+
+        # 更新磚塊.        
+        bricks.update()
+            
+    #顯示磚塊數量.
+    showFont( u"磚塊數量:"+str(brick_num), 8, 20, (255, 0, 0))
+
+    # 秀板子.
+    paddle.rect[0] = paddle_x
+    paddle.update()
+
+    # 碰撞判斷-球碰板子.
+    if(isCollision( ball.pos[0], ball.pos[1], paddle.rect)):        
+        # 球反彈.
+        dy = -dy;         
+            
+    # 球.
+    # 0:等待開球
+    if(game_mode == 0):
+        ball.pos[0] = ball_x = paddle.rect[0] + ( (paddle.rect[2] - ball.radius) >> 1 )
+        ball.pos[1] = ball_y = paddle.rect[1] - ball.radius        
+    # 1:遊戲進行中
+    elif(game_mode == 1):
+        ball_x += dx
+        ball_y += dy
+        #判斷死亡.
+        if(ball_y + dy > canvas_height - ball.radius):
+            game_mode = 0        
+        # 右牆或左牆碰撞.
+        if(ball_x + dx > canvas_width - ball.radius or ball_x + dx < ball.radius):
+            dx = -dx
+        # 下牆或上牆碰撞
+        if(ball_y + dy > canvas_height - ball.radius or ball_y + dy < ball.radius):        
+            dy = -dy
+        ball.pos[0] = ball_x
+        ball.pos[1] = ball_y
+
+    # 更新球.
+    ball.update()
+
+    # 顯示中文.
+    showFont( u"FPS:" + str(int(clock.get_fps())), 8, 2, (255, 0, 0))
+
+    # 更新畫面.
+    pygame.display.update()
+    clock.tick(60)
+
+# 離開遊戲.
 pygame.quit()
+quit()
+
+# encoding: utf-8
+
+#-------------------------------------------------------------------------
+# 畫Box.
+#-------------------------------------------------------------------------
+class Box(object):
+    #-------------------------------------------------------------------------
+    # 建構式.
+    #   pygame    : pygame.
+    #   canvas    : 畫佈.
+    #   name    : 物件名稱.
+    #   rect      : 位置、大小.
+    #   color     : 顏色.
+    #-------------------------------------------------------------------------
+    def __init__( self, pygame, canvas, name, rect, color):
+        self.pygame = pygame
+        self.canvas = canvas
+        self.name = name
+        self.rect = rect
+        self.color = color
+
+        self.visible = True
+        
+    #-------------------------------------------------------------------------
+    # 更新.
+    #-------------------------------------------------------------------------
+    def update(self):
+        if(self.visible):
+            self.pygame.draw.rect( self.canvas, self.color, self.rect)
+
+#-------------------------------------------------------------------------
+# 畫圓.
+#-------------------------------------------------------------------------
+class Circle(object):
+    #-------------------------------------------------------------------------
+    # 建構式.
+    #   pygame  : pygame.
+    #   canvas  : 畫佈.
+    #   name    : 物件名稱.
+    #   pos     : 位置.  
+    #   radius  : 大小.
+    #   color   : 顏色.    
+    #-------------------------------------------------------------------------
+    def __init__( self, pygame, canvas, name, pos, radius, color):
+        self.pygame = pygame
+        self.canvas = canvas
+        self.name = name
+        self.pos = pos
+        self.radius = radius
+        self.color = color
+        
+        self.visible = True
+
+    #-------------------------------------------------------------------------
+    # 更新.
+    #-------------------------------------------------------------------------
+    def update(self):
+        if(self.visible):
+            self.pygame.draw.circle( self.canvas, self.color, self.pos , self.radius)
